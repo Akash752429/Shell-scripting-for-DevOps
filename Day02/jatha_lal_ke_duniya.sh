ce80981da3faf4cf79c8ac4ef1886be24c5b9c8f
Ubuntu <ubuntu@ip-172-31-7-41.us-east-2.compute.internal>
2024-04-11 11:13:33 +0000

    this is new command add

--- Day02/jatha_lal_ke_duniya.sh
+++ Day02/jatha_lal_ke_duniya.sh
@@ -5,6 +5,9 @@
    This is if statement command
 comment
 
+# this is function definition
+
+function is_loyal() {
 read -p "Jetha ne mud ke kisa dekha : " bandi
 read -p "Jetha ka pyaar %" pyaar
 if [[ $bandi == "daya bhabhi" ]];
@@ -16,3 +19,6 @@ then
 else
 	echo "jetha is not loyal"
 fi
+ }
+  # This is a function call
+ is_loyal
